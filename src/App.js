4adcc6220dc79de8119e29451a634d5096286403
Jonathan Ng <ngkjon@gmail.com>
2019-11-27 23:49:09 -0500

    social icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/main'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,10 +16,11 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        > */}
+          <Main/>
+          {/* Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Social from './social';
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div className="page" id="main">
+                <h1 className="txt">Jon. N</h1>
+                <Social></Social>
+            </div>
+            
+        )
+    }
+}
